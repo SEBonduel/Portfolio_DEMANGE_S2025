e41b15577681f876cdc166d55eea25ec9630d27e
SEBonduel <sebastien050599@hotmail.fr>
2025-02-27 03:49:23 +0100

    z-index fix

--- mon-portfolio/src/components/Footer.jsx
+++ mon-portfolio/src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
 
   return (
     <motion.footer
-      className="fixed flex flex-col justify-center w-full  items-center bottom-0 bg-black text-white p-4 text-center backdrop-blur-md shadow-lg h-auto md:h-[150px]"
+      className="fixed flex flex-col justify-center w-full items-center bottom-0 bg-black text-white p-4 text-center backdrop-blur-md shadow-lg h-auto md:h-[150px]"
       initial={{ opacity: 0, y: 50 }}
       animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
       transition={{ duration: 0.6, ease: "easeOut" }}
@@ -27,13 +27,13 @@ const Footer = () => {
       <div className="flex flex-col items-center gap-2 w-full max-w-lg px-4">
         <p className="text-lg md:text-xl font-bold text-neon-blue text-orbitron text-center">Sébastien Demange</p>
         <div className="flex gap-4 text-lg md:text-xl justify-center">
-          <a href="https://github.com/SEBonduel" target="_blank" rel="noopener noreferrer" className="hover:text-green-500 transition">
+          <a href="https://github.com/SEBonduel" target="_blank" rel="noopener noreferrer" className="hover:text-green-500 transition z-[10001]">
             <FaGithub />
           </a>
-          <a href="https://www.linkedin.com/in/s%C3%A9bastien-demange-5a4b03243/" target="_blank" rel="noopener noreferrer" className="hover:text-green-500 transition">
+          <a href="https://www.linkedin.com/in/s%C3%A9bastien-demange-5a4b03243/" target="_blank" rel="noopener noreferrer" className="hover:text-green-500 transition z-[10001]">
             <FaLinkedin />
           </a>
-          <a href="mailto:sebastien050599@hotmail.fr" className="hover:text-green-500 transition">
+          <a href="mailto:sebastien050599@hotmail.fr" className="hover:text-green-500 transition z-[10001]">
             <FaEnvelope />
           </a>
         </div>
